a7e6a56f8be25159d6f56839473d68c79da997d1
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-24 18:18:08 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def update
     # @post.update(post_params)
-    # 
+    #
     # redirect_to post_path(@post)
     @post.update(post_params)
     if @post.valid?
